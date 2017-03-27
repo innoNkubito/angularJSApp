98bb9d39b30d46d387fa94beb268b926c0b663eb
innoNkubito <innocentnkubito92@gmail.com>
2017-03-27 18:24:58 -0500

    Fix bug and add Style to Buy Page

--- App/BuyPage/buyController.js
+++ App/BuyPage/buyController.js
@@ -2,6 +2,10 @@ angular.module("buyModule").controller('buyCtrl', function($scope){
 
 	$scope.productData = JSON.parse(localStorage.getItem("currentGuitar"));
 	$scope.quantity = 1 ;
+
+	$scope.price = parseFloat($scope.productData.price);
+
+	$scope.total = $scope.price * $scope.quantity;
 	
 
 });
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
- #guitarCenter
-# angularJSApp
-# angularJSApp
+ # angularJSApp
+
+1. To run this, see server js. Run on http port 3000 : localhost:3000/#/
